7ff3cd65bd3cb6a7595d3110e9b5c0321a97dbfa
swichblade <lisnih55@gmail.com>
2021-09-26 12:13:42 +0300

    delete @Component from UserListRepository

--- src/main/java/com/example/demo/repository/UserListRepository.java
+++ src/main/java/com/example/demo/repository/UserListRepository.java
@@ -1,13 +1,11 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.userlist.UserListEntity;
 import com.example.demo.entity.user.User;
+import com.example.demo.entity.userlist.UserListEntity;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Component
 public interface UserListRepository extends CrudRepository<UserListEntity, Long> {
     UserListEntity findAllById(Long id);
 
